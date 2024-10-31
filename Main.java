190b9fb151f04a3b83dca8b893ae4191e374cf40
Lucio Plancarte <lplancarte@cnm.edu>
2024-10-31 13:23:47 -0600

    Added ThreadOperation class; Created ThreadOperation objects in Main.java

--- Main.java
+++ Main.java
@@ -25,9 +25,21 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+		//2-Dimensional Test Arrays
+		int [][] blue =
+		{{2,3,1,2,5,1},{3,1,2,2,2,4},{1,2,3,2,7,2},{3,6,1,5,1,3}};
 
-		System.out.println();
+		int [][] red =
+		{{6,5,4,1,4,3},{3,3,2,2,1,1},{7,5,4,3,2,5},{2,1,8,4,8,4}}
 
+		System.out.println("Creating 4 ThreadOperation Objects");
+		//Instantiate 4 ThreadOperation objects
+		ThreadOperation upperRight = new ThreadOperation(blue,red,Quadrant.I);
+		ThreadOperation upperLeft = new ThreadOperation(blue,red,Quadrant.II);
+		ThreadOperation lowerRight = new ThreadOperation(blue,red,Quadrant.III);
+		ThreadOperation lowerLeft = new ThreadOperation(blue,red,Quadrant.IV);
+		//Start them
+		//Join them
 
 
 	}
---
+++ ThreadOperation.java
@@ -0,0 +1,42 @@
+/**
+Programmer: Lucio Plancarte
+Created: 31-OCT-2024
+Description: extends Thread and performs submatrix addition
+*/
+
+/**
+ThreadOperation constructor takes in two matrices and a string to indicate
+quadrant. The result is stored in 2-D array C.
+*/
+public class ThreadOperation extends Thread{
+
+	//Class Variables
+	private int[][] A;
+	private int[][] B;
+	private int[][] C;
+	private enum Quadrant {
+		I,
+		II,
+		III,
+		IV
+	};
+	private Quadrant q;
+	//https://www.w3schools.com/java/java_enums.asp
+
+	//Constructor
+	public ThreadOperation(int[][] A, int[][] B, Quadrant q){
+		this.A = A;
+		this.B = B;
+		this.q = q;
+	}
+
+	@Override
+	public void run(){
+		//TODO: Add quadrants of matrices A and B set to C
+		this.C = new int[0][0];
+	}
+
+	public int[][] getResult(){
+		return this.C;
+	}
+}
