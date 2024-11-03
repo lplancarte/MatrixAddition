6e28b085eb1f4efe7c63fea6bd1ad17aba3dafac
Lucio Plancarte <lplancarte@cnm.edu>
2024-11-03 08:01:47 -0700

    createArray() created. Arrays are now read in from file and assigned to red[][] and blue[][];

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
+/**
+Programmer: Lucio Plancarte
+Modified: 31-OCT-2024
+Description: Use threads to split up matrix addition.
+			Part 1 of 2: Read in numbers from file and fill in arrays
+			Print arrays to terminal
+*/
+
 /*
 This code is provided to give you a
 starting place. It should be modified.
@@ -17,6 +25,44 @@ and connect these issues to
 multi-threading.
 
 */
+/**
+A1: 
+	I am currently working on a truck. Replacing a clutch and all
+	associated parts; seals, gaskets, nuts and bolts. The steps that
+	need to be taken include removing systems such as the exhaust and 
+	transmission. Each one of these processes takes time and effort.
+	As a single mechanic there is only so much that can be accomplished.
+	I think of multi-threading as a way of assigning tasks to separate
+	threads so that a process, especially a multi-step process, can
+	be completed in a more efficient and hopefully faster way. 
+	
+	Splitting a process into smaller parts can allow for a user to continue
+	working with a program without having to wait for different sub processes
+	to complete before continuing. With the analogy above, while the 
+	transmission is being removed, the starter can be removed concurrently.
+	Perhaps a gasket can be replaced while a different process, such
+	as the bolt-removal process is happening in the background.
+
+	Working within oracle databases there were times that tables would need to
+	be locked to prevent other users from potentially altering or modifying
+	data while it was being analyzed. I believe this is an example of 
+	potential pitfall of multi-threading where it is important to keep track 
+	of potential concurrent processes on different threads within a multi-
+	server system. This was handled by the oracle database server with 
+	built in locks.These could be at the row, table or database level.
+
+	I think multi-threading is advantageous within databases; scaling
+	websites with user iteractions; and game development: where waiting
+	on a single thread to finish might makes some games extra laggy.
+	-----------------------------------------------------------------
+	TLDR: Multi-threading breaks up a process into small manageable chunks
+		these threads complete sub-processes and can potentially finish a task
+		more efficiently and quickly. Human beings are horrible at 
+		multi-tasking, machines are not. Analyzing data can be done quicker
+		with multiple threads. Couple hundred rows of data are no big deal;
+		a couple hundred thousand or million(s) and breaking up a task is more 
+		efficient.
+*/
 import java.io.IOException;
 import java.io.File;
 import java.util.Scanner;
@@ -27,27 +73,44 @@ import java.io.FileNotFoundException;
 
 public class Main
 {
-	//2D Array that will hold result
-	private static int [][]purple =
-	{{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0}};
+	//2-Dimensional Test Arrays
+	private static int [][] blue = null;
+	//	{{1,1,1,1,1,1},{2,2,2,2,2,2},{3,3,3,3,3,3},{4,4,4,4,4,4}};
 
-	private static int NUM_ROW  = 0;
-	private static int NUM_COL = 0;
+	private static int [][] red = null;
+	//	{{4,4,4,4,4,4},{3,3,3,3,3,3},{2,2,2,2,2,2},{1,1,1,1,1,1}};
+
+	//2-D result array
+	private static int [][]purple = null;
+	//{{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0},{0,0,0,0,0,0}};
+
+	private static int ROW  = 0;
+	private static int COL = 0;
 	private static String inputFileDefault = "matrix1.txt";
 
 
 	public static void main(String[] args)
 	{
-		//2-Dimensional Test Arrays
-		int [][] blue =
-		{{1,1,1,1,1,1},{2,2,2,2,2,2},{3,3,3,3,3,3},{4,4,4,4,4,4}};
 
-		int [][] red =
-		{{4,4,4,4,4,4},{3,3,3,3,3,3},{2,2,2,2,2,2},{1,1,1,1,1,1}};
+		//File Handling
+		System.out.println("File Handling Start");
+		File inputFile = checkFile(args);
+		if(inputFile == null)
+			return;
+		else
+			processFile(inputFile);
 
+		System.out.println("File Handling End");
 
-		System.out.println("Creating 4 ThreadOperation Objects");
+		System.out.printf("ROW: %d\nCOL: %d\n",ROW, COL);
+		System.out.println("Printing array: blue");
+		print2dArray(blue);
+
+		System.out.println("\nPrinting array: red");
+		print2dArray(red);
+
 		//Instantiate 4 ThreadOperation objects
+		System.out.println("Creating 4 ThreadOperation Objects");
 		ThreadOperation upperRight = new ThreadOperation(blue,red,Quadrant.I);
 		ThreadOperation upperLeft = new ThreadOperation(blue,red,Quadrant.II);
 		ThreadOperation lowerRight = new ThreadOperation(blue,red,Quadrant.III);
@@ -68,24 +131,11 @@ public class Main
 			System.out.println("INTERRUPTION HAS OCCURED");
 		};
 
-		//Print a test array
-		System.out.println("Printing test array: blue");
-		print2dArray(blue);
-
-		System.out.println("\nPrinting test array: red");
-		print2dArray(red);
-
+		//Print purple result array
 		System.out.println("\nPrinting result array: purple");
 		print2dArray(purple);
 
-		//File Handling
-		File inputFile = checkFile(args);
-		if(inputFile == null)
-			return;
-		else
-			processFile(inputFile);
-
-		System.out.printf("ROW: %d\nCOL: %d\n",NUM_ROW, NUM_COL);
+		
 
 	}//end main()
 
@@ -145,13 +195,17 @@ public class Main
 	*/
 	static private void processFile(File inputFile){
 		String line = "";
+		int count = 0;
 		try{
 			Scanner reader = new Scanner(inputFile);
 			line = reader.nextLine(); //get row and col from this line
 			String[] rowCol = line.split(" ");
-			NUM_ROW = Integer.parseInt(rowCol[0]);
-			NUM_COL = Integer.parseInt(rowCol[1]);
+			ROW = Integer.parseInt(rowCol[0]);
+			COL = Integer.parseInt(rowCol[1]);
 
+			//Create Arrays red, blue, and purple,fill with 0's
+			createArrays(ROW,COL);
+			//Fill arrays blue and red with data from file
 			while(reader.hasNextLine()){
 				line = reader.nextLine();
 
@@ -159,20 +213,34 @@ public class Main
 				if(line.isEmpty()){continue;};
 				String[] lineData = line.split(" ");
 
-				for(int i = 0; i < lineData.length; i++){
-					System.out.print(lineData[i]);
+				for(int i = 0; i < COL; i++){
+					if(count < ROW)
+						blue[count][i] = Integer.parseInt(lineData[i]);
+					else
+						red[count -ROW][i] = Integer.parseInt(lineData[i]);
+					//System.out.print(lineData[i]);
 				}
-				System.out.println();
-			}
+				count++;
+				//System.out.println();
+			}//end while
 			//DO SOMETHING POSSIBLY
 		}catch(Exception e){};
 
 	}//end processFile()
 
 
-	//public static int[][] matrixFromFile(int rows,int cols,Scanner 
-	//fileReader){
-	//	return
-	//}
+	/**		createArrays(int rows, int cols)
+	@param int row - number of rows; found in text file
+	@param int col - number of cols; found in text file
+	*This method is a helper method to processFile. After ROW and COL
+	*are read, three arrays are created and filled with 0's with 
+	*appropriate number of rows and columns
+	*/
+	static private void createArrays(int rows, int cols){
+		blue = new int[rows][cols];
+		red = new int[rows][cols];
+		purple = new int[rows][cols];
+	}
+
 
 }//end class Main
